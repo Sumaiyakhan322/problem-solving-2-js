ff3e54c67e64de619cced9a161be3b96d5a02116
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-06 19:22:20 +0600

    added 5-7

--- string_and_function.js
+++ string_and_function.js
@@ -35,3 +35,45 @@ else if(mela>mitu && mela>habin){
 else{
     console.log("habin is the topper");
 }
+
+//5---a function that takes 3 parameters and return max number also min number
+function maxNumber(a,b,c){
+    console.log(Math.max(a,b,c));
+}
+function minNumber(a,b,c){
+    console.log(Math.min(a,b,c));
+}
+maxNumber(23,45,88);
+minNumber(2,8,10)
+//6-------- a function that takes a array and find the tallest person
+function findTheTallest(arr){
+    let largest=arr[0];
+    for(i=0;i<arr.length;i++){
+        let element=arr[i];
+        if(element>largest){
+            largest=element;
+        }
+    }
+    return largest;
+    
+
+}
+const heights=[12,46,767,645,444,100];
+const tallest=findTheTallest(heights);
+console.log("The tallest: ", tallest);
+
+////7-------- a function that takes a array and find the lowest person
+function findLowest(arr){
+    let lowest=arr[0];
+    for(i=0;i<arr.length;i++){
+        let element=arr[i];
+        if(element<lowest){
+            lowest=element;
+        }
+
+    }
+    return lowest;
+}
+const height2=[20,48,459,9,67,3];
+const lowestHeight=findLowest(height2);
+console.log("The lowest is: ",lowestHeight);
